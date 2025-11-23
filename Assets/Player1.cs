c68170849bf21eb6730cbed88488621fe6541fd3
GiulianoMauroDana <104393352+GiulianoMauroDana@users.noreply.github.com>
2025-11-23 18:38:04 -0300

    23/11/25
    vida del personaje
    menu

--- Assets/Player1.cs
+++ Assets/Player1.cs
@@ -2,14 +2,17 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player1 : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI titleText;
     [SerializeField] private TextMeshProUGUI subtitleText;
+    [SerializeField] private int health;
+    [SerializeField] private Image barHealth;
     private static int _score;
-    
+    private static int _damage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,18 +24,25 @@ public class Player1 : MonoBehaviour
     {
         WinCondision();
         Reiniciar();
+        ApplyDamage();
     }
     private void OnEnable()
     {
         Enemy.enemyKill += CountPoints;
-        
+        Enemy.enemyDamage += CountDamage;
     }
     private void OnDisable()
     {
         Enemy.enemyKill -= CountPoints;
-        
+        Enemy.enemyDamage -= CountDamage;
     }  
     
+    private void CountDamage(int damage)
+    {
+        _damage += damage;
+        UpdateUI();
+    }
+
     private void CountPoints(int scoreEnter)
     {
         _score+=scoreEnter;
@@ -41,7 +51,20 @@ public class Player1 : MonoBehaviour
     private void UpdateUI()
     {
         scoreText.text = "Score:" + _score.ToString();
+        barHealth.fillAmount = health / 100f;
     }
+
+    private void ApplyDamage()
+    {
+        health-= _damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            UpdateUI();
+        }
+    }
+
     private void WinCondision()
     {
         if (_score == 4)
